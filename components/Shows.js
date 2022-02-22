13c43232e3f9e4faa0e29a82bbbe2561959cbafe
Ademayowa <ademayowaadeniyi@gmail.com>
2022-02-22 19:18:40 +0100

    added popular movies

---
+++ components/Shows.js
@@ -0,0 +1,7 @@
+export default function Shows() {
+  return (
+    <div>
+      <h2>TV shows</h2>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
 import Hero from '@/components/Hero';
 import Layout from '@/components/Layout';
+import Movies from '@/components/Movies';
+import { BASE_URL } from '@/config/index';
 
-export default function HomePage() {
+export default function HomePage({ popularMovies }) {
   return (
     <Layout title='Movie Trailers | Home'>
       <Hero />
+      <Movies movies={popularMovies} title="What's Popular" />
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const [popularMoviesRes, popularShowsRes, topRatedShowsRes] =
+    await Promise.all([
+      fetch(
+        `${BASE_URL}/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      ),
+
+      fetch(
+        `${BASE_URL}/tv/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      ),
+
+      fetch(
+        `${BASE_URL}/tv/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      ),
+    ]);
+
+  const [popularMovies, popularShows, topRatedShows] = await Promise.all([
+    popularMoviesRes.json(),
+    popularShowsRes.json(),
+    topRatedShowsRes.json(),
+  ]);
+
+  return {
+    props: {
+      popularMovies: popularMovies?.results,
+      popularShows: popularShows?.results,
+      topRatedShows: topRatedShows?.results,
+    },
+  };
 }

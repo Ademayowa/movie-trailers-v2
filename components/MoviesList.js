4003dd9784072e3f0d91d26b5c1f50ebb4326476
Ademayowa <ademayowaadeniyi@gmail.com>
2022-02-23 15:22:41 +0100

    added loading & working on single movie

--- components/MoviesList.js
+++ components/MoviesList.js
@@ -8,6 +8,7 @@ export default function MoviesList({ movie }) {
   return (
     <div className='flex mt-2 md:mt-5 mb-5 min-w-[250px] min-h-[170px] md:min-w-[300px] md:min-h-[400px] rounded-lg overflow-hidden cursor-pointer transform hover:scale-105 transition duration-300'>
       <Image
+        onClick={() => router.push(`/movie/${movie.id}`)}
         src={
           `${IMAGE_URL}${movie.poster_path}` ||
           `${IMAGE_URL}${movie.backdrop_path || movie.poster_path}`

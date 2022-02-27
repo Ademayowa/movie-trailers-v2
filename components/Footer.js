d3f8b2da1da6a3406cb4549cf32160ee2cb8e00e
Ademayowa <ademayowaadeniyi@gmail.com>
2022-02-27 16:28:20 +0100

    changed body color

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <div className=' px-10 md:px-12 text-dark text-center mt-20 mb-4'>
+    <div className=' px-10 md:px-12 text-white text-center mt-20 mb-4'>
       <p>
-        Powered by TMDB API,{' '}
+        Powered by TMDB-API.{' '}
         <a className='font-bold' href='https://mayowa.dev'>
           Built by Mayorstacks
         </a>
